00803a05f91c28af5f823d11ea47100d09505419
Kyle Smith <tman2kyle@hotmail.com>
2015-10-19 16:47:50 -0500

    fb nonsense

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("This is a test");
+//   FB.api(
+//     "/me",
+//     function (response) {
+//       if (response && !response.error) {
+//         // console.log("the information back from facebook is ",response);
+//       }
+//     }
+// );
   res.render('index', { title: 'Express' });
 });
 
