d64c1e248d8e40503d0f874f3c17b874e8581911
Kyle Smith <tman2kyle@hotmail.com>
2015-10-21 15:34:44 -0500

    user can add things to database

--- routes/users.js
+++ routes/users.js
@@ -11,34 +11,75 @@ router.get('/users', function(req, res, next) {
 });
 
 var passThisAlong = {};
-    
+router.post('/createe', function(req, res, next) {
+  var name = req.body.name;
+  var waitTime = req.body.waitTime;
+  var drinkType = req.body.drinkType;
+
+  var beerWaitTime = null;
+  var cocktailWaitTime = null;
+  var wineWaitTime = null;
+  var liquorWaitTime = null;
+
+  if (drinkType === "beer")
+  {
+    beerWaitTime = waitTime;
+  }
+  else if (drinkType === "wine")
+  {
+    wineWaitTime = waitTime;
+  }
+  else if (drinkType === "liquor")
+  {
+    liquorWaitTime = waitTime;
+  }
+  else if (drinkType === "cocktail")
+  {
+    cocktailWaitTime = waitTime;
+  }
+
+  var newBar = Bar({
+      name: name,
+      comments: drinkType,
+      users: [{type_wait_time : {
+        beer : beerWaitTime,
+        cocktail : cocktailWaitTime,
+        wine : wineWaitTime,
+        liquor : liquorWaitTime
+      }}]
+      //.users[0].type_wait_time.beer
+  });
+
+  newBar.save(function(err) {
+      if (err) console.log(err);
+  });
+  res.redirect('/users')
+}); 
 router.post('/test/:searchedItem', function(req, res, next) {
     var barQuery = req.params.searchedItem;
-    Bar.find({ name: barQuery},  function(err, user) {
+    Bar.find({ name: barQuery},  function(err, documents) {
       if (err) console.log(err);
-      var searchedBar = user[0];
-      console.log("name: ",searchedBar.name,"average wait: ",searchedBar.avg_wait_time);
+      var searchedBar = documents[0];
+      console.log("name: ",searchedBar.name,"average wait: ",searchedBar.avg_wait_time, " Other thing ",searchedBar.users[0].type_wait_time.beer)
       //  type_wait_time: { liquor: null, wine: null, cocktail: null, beer: 2 },
     // avg_wait_time: 2,
     // address: '709 E 6th St, Austin, TX 78701',
     // name:
        res.render('users',{title: passThisAlong, name:searchedBar.name, avgWaitTime: searchedBar.avg_wait_time});
     });
-
 });
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
   Bar.find({}, function(err, documents) {
-  if(err) {
-    console.log("The error is ",err);
-    throw err;
-  } else {
+    if(err) {
+      console.log("The error is ",err);
+      throw err;
+    } else {
 
-    passThisAlong = documents;
-  }
-    res.render('users', { title: passThisAlong, name: "", avgWaitTime:""});
-});
-
+      passThisAlong = documents;
+    }
+      res.render('users', { title: passThisAlong, name: "", avgWaitTime:""});
+  });
 });
 
 module.exports = router;
