99c91e368f58460fd46406289971b078c4ce92d6
Kyle Smith <tman2kyle@hotmail.com>
2015-10-20 19:19:58 -0500

    you can see stuff from the db to the screen

--- models/bar.js
+++ models/bar.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var barSchema = mongoose.Schema ({
-	name: String,
-	address: String,
-	avg_wait_time: Number,
-	users: Array,
-	type_sold: Array
+    name: String,
+    address: String,
+    avg_wait_time: Number,
+    users: Array,
+    type_sold: Array
 });
 
 //Model: creates the collection with document structure
@@ -14,9 +14,3 @@ var Bar = mongoose.model('Bar', barSchema);
 
 //Document: creates a new document in the collection
 module.exports = Bar;
-//Save to database:
-
-
-//date.now
-
-
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+// Require the User model
+var Bar = require('../models/user');
+// var Bar = require('./models/user')
+
 /* GET users listing. */
 router.get('/users', function(req, res, next) {
   res.render('users');
 });
 
+var passThisAlong = {};
+
 
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  res.render('users', { title: 'Express' });
+  Bar.find({}, function(err, documents) {
+  if(err) {
+    console.log("The error is ",err);
+    throw err;
+  } else {
+    console.log("Testing barrrrrrr", documents);
+    passThisAlong = documents;
+  }
+});
+  res.render('users', { title: passThisAlong});
 });
 
 module.exports = router;
